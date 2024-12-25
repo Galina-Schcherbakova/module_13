a4e19a29668a3e046890936e5da96dfc7fe67952
Galina-Schcherbakova <galchonisch@mail.ru>
2024-12-25 22:34:28 +0300

    module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -1,52 +1,45 @@
+import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
 
+logging.basicConfig(level=logging.INFO)
 
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
-
-API_TOKEN = '7789653976:AAHvzizuR1Tgnbp62o5UcBL2wF66mggpMCc'
+API_TOKEN = ' '
 
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-
 @dp.message_handler(commands=['start', 'help'])
 async def cmd_start(message: types.Message):
-    await message.reply("Чтобы начать, введите 'Calories'.")
-
+    await message.reply('Привет! Я бот, помогающий твоему здоровью. Чтобы начать, введите "Calories".')
 
 @dp.message_handler(lambda message: message.text == 'Calories')
 async def set_age(message: types.Message):
     await UserState.age.set()
     await message.reply("Введите свой возраст:")
 
-
-@dp.message_handler(state=UserState.age)
-
+@dp.message_handler(state=User State.age)
 async def set_growth(message: types.Message, state: FSMContext):
     await state.update_data(age=message.text)
     await UserState.growth.set()
     await message.reply("Введите свой рост:")
 
-
-@dp.message_handler(state=UserState.growth)
-
+@dp.message_handler(state=User State.growth)
 async def set_weight(message: types.Message, state: FSMContext):
     await state.update_data(growth=message.text)
     await UserState.weight.set()
     await message.reply("Введите свой вес:")
 
-
-@dp.message_handler(state=UserState.weight)
-
+@dp.message_handler(state=User State.weight)
 async def send_calories(message: types.Message, state: FSMContext):
     await state.update_data(weight=message.text)
 
@@ -60,6 +53,10 @@ async def send_calories(message: types.Message, state: FSMContext):
 
     await state.finish()
 
+@dp.message_handler(lambda message: True)
+async def all_messages(message: types.Message):
+    await message.reply('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
+    print("Запуск бота...")
     executor.start_polling(dp, skip_updates=True)
